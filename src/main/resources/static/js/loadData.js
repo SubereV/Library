82842a60b81c2bccac4091033776e800c6856323
Vo Nhu Khang <kinvo5120@gmail.com>
2020-08-31 21:54:41 +0700

    Categorize completed

--- src/main/resources/static/js/loadData.js
+++ src/main/resources/static/js/loadData.js
@@ -1,23 +1,14 @@
 function doGetJSON(id) {
 	var url = "/cate/" + id;
-	// Call fetch(url) with default options.
-	// It returns a Promise object:
 	var aPromise = fetch(url);
 
 	// Work with Promise object:
 	aPromise
 		.then(function(response) {
-			console.log("OK! Server returns a response object:");
-			console.log(response);
-
 			if (!response.ok) {
 				throw new Error("HTTP error, status = " + response.status);
 			}
-			// Get JSON Promise from response object:
-			var myJSON_promise = response.json();
-
-			// Returns a Promise object.
-			return myJSON_promise;
+			return response.json();
 		})
 		.then(function(myJSON) {
 			console.log("OK! JSON:");
@@ -25,46 +16,40 @@ function doGetJSON(id) {
 			document.getElementById("count").innerHTML = myJSON.length + "";
 			var element = document.getElementById("books");
 			element.querySelectorAll('*').forEach(n => n.remove());
+			document.getElementById("pageNumbers1").querySelectorAll('*').forEach(n => n.remove());
 			myJSON.forEach(ob => {
 				element.appendChild(blockElement(ob));
-			})
+			});
+
+
+
+			$("#books").pagify(6, ".items");
 		})
 		.catch(function(error) {
-			console.log("Noooooo! Something error:");
-			console.log(error);
 		});
 
 }
+
 function blockElement(data) {
-	/*var element = "<div class=\'col-lg-4 col-md-6 col-sm-6\'>";
-	element += '<div class=\"product__item\">';
-	element += '<div class=\"product__item__pic set-bg\" th:attr=\"data-setbg=\'/img/book/\'' + data.id + '\'.jpg\'\"></div>';
-	element += '<div class=\"product__item__text\">';
-	element += "<h6> < a href=\'book?id="+ data.id +"\'></a >"+ data.title +" </h6 > ";
-	*/
 	var element = document.createElement("div");
-	element.className = "col-lg-4 col-md-6 col-sm-6";
+	element.className = "col-lg-4 col-md-6 col-sm-6 items";
 	var element1 = document.createElement("div");
 	element1.className = "product__item";
 	var bpic = document.createElement("div");
 	bpic.className = "product__item__pic set-bg";
-
 	bpic.setAttribute("data-setbg", '/img/book/' + data.id + '.jpg');
-	bpic.setAttribute("style", "background-image: url(\"/img/book/"+data.id+".jpg\");");
+	bpic.setAttribute("style", "background-image: url(\"/img/book/" + data.id + ".jpg\");");
 	element1.appendChild(bpic);
 	var text = document.createElement("div");
 	text.className = "product__item__text";
 	var title = document.createElement("h6");
-	var link =	document.createElement("a");
-	link.href="/book?id="+data.id;
-	link.innerText=data.title;
+	var link = document.createElement("a");
+	link.href = "/book?id=" + data.id;
+	link.innerText = data.title;
 	title.appendChild(link);
 	text.appendChild(title);
 	element1.appendChild(text);
-	
 	var li1 = document.createElement("li");
-
-	
 	element.appendChild(element1);
 	return element;
 }
